9a7adbb3b933c9bb5b39612bdb5501fd5b84413c
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-04-23 01:10:22 -0400

    Fix Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -31,9 +31,9 @@ const Navbar = props => {
                                         <li className=""><Link to="/configuracion"><i className="fa fa-cogs"></i> Configuracion</Link></li>
                                     )
                             }
-                            <li className="reverse"><Link to="/registro"><i className="fa fa-pencil-square-o"></i> Registrarse</Link></li>
-                            <li className=""><Link to="/notificar"><i className="fa fa-bell-o"></i> Notificar</Link></li>
-                            <li className=""><Link to="/contacto"><i className="fa fa-envelope"></i> Contacto</Link></li>
+                            <li className="reverse"><Link data-toggle="collapse" data-target="#navbar-collapse-1" to="/registro"><i className="fa fa-pencil-square-o"></i> Registrarse</Link></li>
+                            <li className=""><Link to="/notificar" data-toggle="collapse" data-target="#navbar-collapse-1"><i className="fa fa-bell-o"></i> Informa Aquí</Link></li>
+                            <li className=""><Link to="/contacto" data-toggle="collapse" data-target="#navbar-collapse-1"><i className="fa fa-envelope"></i> Contacto</Link></li>
                         </ul>
                     </div>
 
