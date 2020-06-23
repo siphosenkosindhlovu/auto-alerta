50cf5f330e2bc70c72cd5fd86e4b32ad7d39f3e5
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2020-06-23 14:41:15 +0200

    replaced old stylesheets with bootstrap 4 and Sass and added absolute file import support

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import 'bootstrap3/dist/css/bootstrap-theme.min.css';
-import 'bootstrap3/dist/css/bootstrap.min.css';
-import 'bootstrap-toggle/css/bootstrap-toggle.min.css'
-import '@fortawesome/fontawesome-free/css/all.css';
-import './index.css';
-import Layout from './layout';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Layout />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// import 'bootstrap3/dist/css/bootstrap.min.css';
+// import 'bootstrap-toggle/css/bootstrap-toggle.min.css';
+// import '@fortawesome/fontawesome-free/css/all.css';
+// import './index.css';
+import 'scss/main.scss';
+import App from 'App';
+ReactDOM.render(<App />, document.getElementById('root'));
