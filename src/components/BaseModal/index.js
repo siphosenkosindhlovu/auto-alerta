b2c16b8fc67f99b33e63e433b74ae10bdccd1b44
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2020-07-09 15:42:50 +0200

    Added error modal variant

--- src/components/BaseModal/index.js
+++ src/components/BaseModal/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import check from 'images/check-VB_Mesa de trabajo 1.svg';
+import error from 'images/ic-error_Mesa de trabajo 1.svg';
 import PropTypes from 'prop-types';
 export default function BaseModal({
   title,
@@ -9,14 +10,15 @@ export default function BaseModal({
   show,
   handleClose,
   dismissButtonText,
+  success,
   ...props
 }) {
   return (
     <Modal show={show} onHide={handleClose} {...props} centered>
       <Modal.Header>
         <img
-          src={check}
-          alt="Success"
+          src={success ? check : error}
+          alt={success ? 'Success Icon' : 'Error Icon'}
           style={{ width: '1.5rem', height: '1.5rem' }}
           className="mr-3"
         />
